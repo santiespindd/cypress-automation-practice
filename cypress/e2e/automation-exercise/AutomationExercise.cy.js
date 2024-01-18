83b78bba38e49c1cee1c63647c572a54e8dde61d
santiespindd <santiagonespindola@gmail.com>
2024-01-18 16:21:08 -0300

    Finished: login and sign up Test cases

--- cypress/e2e/automation-exercise/AutomationExercise.cy.js
+++ cypress/e2e/automation-exercise/AutomationExercise.cy.js
@@ -2,27 +2,33 @@
 
 describe('Automation Exercise' , ()=>{
 
-    before(()=>{
+   
 
-        cy.visit('https://automationexercise.com/');
+    beforeEach(() => {
 
-        cy.fixture('credentials').then( function (testdata){
+      cy.visit('https://automationexercise.com/');
+
+      cy.fixture('credentials').then( function (testdata){
             this.testdata = testdata;
         })
 
-    })
+      cy.log('Should have a title')
 
+            cy.title().should('eq', 'Automation Exercise');
+
+      cy.log('Navigate to sign up page');
+
+            cy.get('.shop-menu > .nav > :nth-child(4) > a').click();
+
+    });
+    
+    
 
     it('Test Case 1: Register User', function() {
+
         
 
-        cy.log('Should have a title')
-
-              cy.title().should('eq', 'Automation Exercise');
-
-        cy.log('Navigate to sign up page');
-
-              cy.get('.shop-menu > .nav > :nth-child(4) > a').click()
+      
 
         cy.log('Verify New User Signup! is visible')
               
@@ -78,10 +84,7 @@ describe('Automation Exercise' , ()=>{
                
                   cy.get(':nth-child(10) > a').should('include.text', 'Logged in as ' + this.testdata.name)
        
-      cy.log('Deleted account')
-                  cy.get('.shop-menu > .nav > :nth-child(5) > a').click()
-                  cy.get('b').should('have.text', 'Account Deleted!')
-                  cy.get('[data-qa="continue-button"]').click()
+      
 
 
 
@@ -89,7 +92,79 @@ describe('Automation Exercise' , ()=>{
 
     });
     
+   it('Test Case 2: Login User with correct email and password', function() {
+
+      cy.get('.login-form > h2').should('contain','Login to your account');
+      cy.get('[data-qa="login-email"]').type(this.testdata.email);
+      cy.get('[data-qa="login-password"]').type(this.testdata.password);
+      
+      cy.get('[data-qa="login-button"]').click();
+      cy.get(':nth-child(10) > a').should('include.text', 'Logged in as ' + this.testdata.name);
+
+   });
    
- 
+   it('Test Case 3: Login User with incorrect email and password', function() {
+      cy.get('.login-form > h2').should('contain.text','Login to your account');
+
+      cy.get('[data-qa="login-email"]').type(this.testdata.incorrect_mail);
+      cy.get('[data-qa="login-password"]').type(this.testdata.password);
+      
+      cy.get('[data-qa="login-button"]').click();
+
+      cy.get('.login-form').should('contain.text','Your email or password is incorrect!');
+   });
+
+   it('Test Case 4: Logout User', function() {
+
+      cy.get('.login-form > h2').should('contain','Login to your account');
+      cy.get('[data-qa="login-email"]').type(this.testdata.email);
+      cy.get('[data-qa="login-password"]').type(this.testdata.password);
+      
+      cy.get('[data-qa="login-button"]').click();
+      cy.get(':nth-child(10) > a').should('include.text', 'Logged in as ' + this.testdata.name);
+
+      cy.get('.shop-menu > .nav > :nth-child(4) > a').click();
+
+      /*I verify button name and url*/
+      cy.get('.shop-menu > .nav > :nth-child(4) > a').should('contain.text','Signup / Login')
+      cy.url().should('include', '/login');
+      
+   });
+
+   it('Test Case 5: Register User with existing email', function() {
+      cy.log('Verify New User Signup! is visible')
+              
+      cy.get('.signup-form > h2').should('have.text' , 'New User Signup!')
+
+      cy.log('Enter Name and Email and Submit')
+
+      cy.get('[data-qa="signup-name"]').type(this.testdata.name)
+      cy.get('[data-qa="signup-email"]').type(this.testdata.email)
+      cy.get('[data-qa="signup-button"]').click()
+
+      cy.get('.signup-form').should('contain.text','Email Address already exist!');
+
+   });
+
+   it('Test Case 0: Delete Account', function() {
+
+      cy.get('.login-form > h2').should('contain','Login to your account');
+      cy.get('[data-qa="login-email"]').type(this.testdata.email);
+      cy.get('[data-qa="login-password"]').type(this.testdata.password);
+      
+      cy.get('[data-qa="login-button"]').click();
+      cy.get(':nth-child(10) > a').should('include.text', 'Logged in as ' + this.testdata.name);
+
+
+      cy.log('Deleted account')
+                  cy.get('.shop-menu > .nav > :nth-child(5) > a').click();
+                  cy.get('b').should('have.text', 'Account Deleted!');
+                  cy.get('[data-qa="continue-button"]').click();
+      
+   });
+
+   
+
+
 
 })
